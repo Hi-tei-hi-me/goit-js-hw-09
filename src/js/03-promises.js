3f1c40c500013028f663428ad82e752286be57a6
Hi-tei-hi-me <lethargium@gmail.com>
2022-10-22 21:49:52 +0300

    Task 3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const delay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+  const amount = Number(formData.get('amount'));
+
+  for (let position = 0; position < amount; position += 1) {
+    createPromise(position, delay + position * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', handleSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const data = { position, delay };
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(data);
+      } else {
+        reject(data);
+      }
+    }, delay);
+  });
 }
