6c5ea407067f39ca85ac53a5e5090269e84cf7ab
Hi-tei-hi-me <lethargium@gmail.com>
2022-10-22 17:12:02 +0300

    Task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+class ColorChanger {
+  constructor() {
+    this.intervalID = null;
+  }
+
+  start() {
+    changeColor();
+    refs.stopBtn.disabled = false;
+    refs.startBtn.disabled = true;
+    this.intervalID = setInterval(() => {
+      changeColor();
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalID);
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+  }
+}
+
+const colorChanger = new ColorChanger();
+
+refs.startBtn.addEventListener('click', () => {
+  colorChanger.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  colorChanger.stop();
+});
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
